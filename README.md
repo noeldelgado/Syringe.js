1278f9555d32802126a79fc0c2e8674961c69871
Noel Delgado <auhcsei@gmail.com>
2014-04-17 10:13:18 -0500

    init commit

--- README.md
+++ README.md
@@ -1,4 +1,115 @@
-Syringe.js
-==========
+# Syringe.js
 
-Injects CSS
+Injects CSS...
+
+## Usage Examples
+
+## Basic
+```
+var props = {
+    body: {
+        background: '#222',
+        color: 'rgba(255,255,255,0.9)'
+    },
+    '.demo p': {
+        'font-size': "6px",
+        fontFamily: "sans-serif"
+    }
+};
+
+Syringe.inject(props);
+```
+
+## Media Queries
+```
+var props = {
+    "@media screen" : {
+        "*" : {
+            fontFamily: "sans-serif"
+        }
+    },
+    "@media all and (min-width: 500px)": {
+        "body": {
+            "background": "lime"
+        }
+    }
+};
+Syringe.inject(props);
+```
+
+## @keyframes
+```
+var props = {
+    '@keyframes anim-test': {
+        from: {
+            color: 'red',
+            boxShadow: "0 0 20px red",
+            transform: 'translate3d(0, -20px, 0)'
+        },
+        to: {
+            color: 'lime',
+            boxShadow: "0 0 20px lime",
+            transform: 'translate3d(0, 20px, -100px)'
+        }
+    },
+    'h1': {
+        position: 'relative',
+        borderRadius: '10px',
+        background: '#222',
+        padding: '10px 20px',
+        animation: 'anim-test 2s ease 0 infinite alternate forwards'
+    }
+};
+
+Syringe.inject(props);
+```
+
+
+## -prefixes?
+
+Non-support by default, however you can define what properties and which prefixes to apply, just extend `Syringe.config.prefixedProperties`  before injection:
+
+```
+Syringe.config.prefixedProperties = {
+    'transform'     : ['webkit', 'moz', 'ms', 'o'],
+    'perspective'   : ['webkit'],
+    'animation'     : ['webkit'],
+    'transform-style': ['webkit', 'ms'],
+};
+```
+
+```
+var props = {
+    'body': {
+        perspective: '1000px',
+        transformStyle: 'preserve-3d',
+        animation: 'animation-test 2s ease 0 0'
+    },
+    'h1': {
+        transform: 'translate3d(0, 0, -1000px)'
+    }
+};
+
+Syringe.inject(props);
+```
+
+That will produce:
+
+```
+body{
+    -webkit-perspective: 1000px;
+    perspective: 1000px;
+    -webkit-transform-style: preserve-3d;
+    -ms-transform-style: preserve-3d;
+    transform-style: preserve-3d;
+    -webkit-animation: animation-test 2s ease 0 0;
+    animation: animation-test 2s ease 0 0;
+}
+h1{
+    -webkit-transform: translate3d(0, 0, -1000px);
+    -moz-transform: translate3d(0, 0, -1000px);
+    -ms-transform: translate3d(0, 0, -1000px);
+    -o-transform: translate3d(0, 0, -1000px);
+    transform: translate3d(0, 0, -1000px);
+}
+ ```
---
+++ src/syringe.js
@@ -0,0 +1,117 @@
+/* Syringe.js -v 0.1.0 */
+
+window.Syringe = {
+
+    config : {
+        prefixedProperties : null
+    },
+    _styleElement : null,
+    _reCamelCase : new RegExp("[A-Z]"),
+    _isEmptyObject : function _isEmptyObject(obj) {
+        for (var prop in obj) {
+            if (obj.hasOwnProperty(prop))
+                return false;
+        }
+        return true;
+    },
+
+    inject : function inject(options) {
+        var self, CSString, iterate;
+
+        self = this;
+        CSString = "";
+
+        if (self._styleElement === null) {
+            self._styleElement = document.createElement('style');
+            self._styleElement.setAttribute("type", "text/css");
+            document.body.appendChild(self._styleElement);
+        }
+
+        iterate = function iterate(obj) {
+            var property, selector;
+
+            for (property in obj) {
+                if (obj.hasOwnProperty(property)) {
+                    if (typeof obj[property] === "object") {
+                        CSString += property + self._openingBracket();
+
+                        if (self._isEmptyObject(obj[property])) {
+                            CSString += self._closingBracket();
+                        } else {
+                            iterate(obj[property]);
+                            CSString += self._closingBracket();
+                        }
+                    } else {
+                        selector = property;
+
+                        if (self._reCamelCase.test(property) === true) {
+                            selector = self._toDash(property);
+                        }
+
+                        if (self.config.prefixedProperties) {
+                            CSString += self._getPrefixedRules(selector, obj[property]);
+                        } else {
+                            CSString += this._formattedRule(selector, obj[property]);
+                        }
+                    }
+                }
+            }
+        };
+
+        iterate(options);
+
+        self._styleElement.innerHTML += CSString;
+    },
+
+    _formattedRule : function _formattedRule(selector, value) {
+        return "\t" + selector + ":" + value + ";\n";
+    },
+
+    _openingBracket : function _openingBracket() {
+        return "{\n\t";
+    },
+
+    _closingBracket : function _closingBracket() {
+        return "\t}\n";
+    },
+
+    _toDash : function _toDash(string) {
+        return string.replace(/([A-Z])/g, function(letter) {
+            return '-' + letter.toLowerCase();
+        });
+    },
+
+    _getPrefixedRules : function _getPrefixedRules(selector, value) {
+        var result, index, prefixed_selectors, prefixed_selectors_length;
+
+        if (this.config.prefixedProperties[selector]) {
+            result = "";
+            prefixed_selectors = this._getPrefixedSelector(selector);
+            prefixed_selectors_length = prefixed_selectors.length;
+
+            for (index = 0; index < prefixed_selectors_length; index +=1) {
+                result += this._formattedRule(prefixed_selectors[index], value);
+            }
+
+            return result;
+        };
+
+        return this._formattedRule(selector, value);
+    },
+
+    _getPrefixedSelector : function _getPrefixedSelector(selector) {
+        var index, result, prefixes, prefixes_length;
+
+        result = [];
+        prefixes = this.config.prefixedProperties[selector];
+        prefixes_length = prefixes.length;
+
+        for (index = 0; index < prefixes_length; index += 1) {
+            result.push('-' + prefixes[index] + '-' + selector);
+        }
+
+        result.push(selector);
+
+        return result;
+    }
+};
